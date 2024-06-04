5e1c68aac596b7575d43f6fefd0b69f53bc3665d
ChaudhryYahya <yahya.chaudhry@student.howest.be>
2024-06-04 14:45:01 +0200

    testset with new model

--- AI/testsettest.py
+++ AI/testsettest.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import os
 
 # Load the trained YOLOv8 model
-model = YOLO('runs/detect/yolov8_new_bottle/weights/best.pt')  # Path to your trained model
+model = YOLO('C:\\Users\\yahya\\Documents\\project_one\\2023-2024-projectone-ctai-yahyasultanch\\runs\\detect\\yolov8_latest_model_bottlecap\\weights\\best.pt')  # Path to your trained model
 
 # Path to the test images
 test_images_path = 'C:\\Users\\yahya\\Documents\\project_one\\2023-2024-projectone-ctai-yahyasultanch\\labelled_dataset(2)\\test\\images'
