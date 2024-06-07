74320944de106b2d5364719788f7324556180e88
ChaudhryYahya <yahya.chaudhry@student.howest.be>
2024-06-07 16:24:48 +0200

    new model

--- AI/model_train.py
+++ AI/model_train.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')  # Load a pretrained model
 
 # Train the model
-model.train(data='C:\\Users\\yahya\\Documents\\project_one\\2023-2024-projectone-ctai-yahyasultanch\\labelled_dataset(2)\\data.yaml', epochs=100, imgsz=640, batch=16 , name='yolov8_latest_model_bottlecap')
+model.train(data='C:\\Users\\yahya\\Documents\\project_one\\2023-2024-projectone-ctai-yahyasultanch\\labelled_dataset(3)\\data.yaml', epochs=100, imgsz=640, batch=16 , name='yolov8_latest_bottle_and_cap')
 
 # Validate the model
 model.val()
