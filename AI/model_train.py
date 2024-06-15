4fa8391438cea7565c9d1fb3a825620e43155522
ChaudhryYahya <yahya.chaudhry@student.howest.be>
2024-06-16 00:25:54 +0200

    new model

--- AI/model_train.py
+++ AI/model_train.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')  # Load a pretrained model
 
 # Train the model
-model.train(data='C:\\Users\\yahya\\Documents\\project_one\\2023-2024-projectone-ctai-yahyasultanch\\labelled_dataset(3)\\data.yaml', epochs=100, imgsz=640, batch=16 , name='yolov8_latest_bottle_and_cap')
+model.train(data='C:\\Users\\yahya\\Documents\\project_one\\2023-2024-projectone-ctai-yahyasultanch\\lat-lab-dataset\\data.yaml', epochs=100, imgsz=640, batch=16 , name='yolov8_last-bot-cap-model')
 
 # Validate the model
 model.val()
--- AI/testsettest.py
+++ AI/testsettest.py
@@ -2,10 +2,10 @@ from ultralytics import YOLO
 import os
 
 # Load the trained YOLOv8 model
-model = YOLO('C:\\Users\\yahya\\Documents\\project_one\\2023-2024-projectone-ctai-yahyasultanch\\runs\\detect\\yolov8_latest_bottle_and_cap\\weights\\best.pt')  # Path to your trained model
+model = YOLO('C:\\Users\\yahya\\Documents\\project_one\\2023-2024-projectone-ctai-yahyasultanch\\runs\\detect\\yolov8_last-bot-cap-model\\weights\\best.pt')  # Path to your trained model
 
 # Path to the test images
-test_images_path = 'C:\\Users\\yahya\\Documents\\project_one\\2023-2024-projectone-ctai-yahyasultanch\\labelled_dataset(3)\\test\\images'
+test_images_path = 'C:\\Users\\yahya\\Documents\\project_one\\2023-2024-projectone-ctai-yahyasultanch\\lat-lab-dataset\\test\\images'
 
 # Get all test images
 test_images = [os.path.join(test_images_path, img) for img in os.listdir(test_images_path) if img.endswith(('.png', '.jpg', '.jpeg'))]
