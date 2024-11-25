7f111a892a41f448d51d71703b33ce2304e42a19
ChaudhryYahya <yahya.chaudhry@student.howest.be>
2024-11-25 21:46:08 +0100

    final commit; project completion

--- neopixel.py
+++ neopixel.py
@@ -1,21 +1,3 @@
-# import time
-# # import py_neopixel_spidev as neopixel
-# from lib import neopixel_spidev as neopixel
-# # Init 8 LEDs on SPI bus 0, cs 0 with colors ordered green, red, blue
-# with neopixel.NeoPixelSpiDev(0, 0, n=8, pixel_order=neopixel.GRB) as pixels:
-#     try:
-#         while True: 
-#             for i in range(8):
-#                 pixels._set_item(i, 255,0,0,255)
-#                 pixels.show()
-#                 time.sleep(1)
-#                 pixels.fill(0)
-#                 time.sleep(0.5)
-#                 # fill all colors with R 127, G 255, B 0
-#                 pixels.fill( 127 << 16 | 255 << 8 | 0)
-#     except KeyboardInterrupt:
-#         pass
-
 import time
 # import py_neopixel_spidev as neopixel
 from lib import neopixel_spidev as neopixel
